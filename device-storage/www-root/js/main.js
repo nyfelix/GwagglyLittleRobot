b6d7eaa7f2c8dbeb373956642da37df9bfdc5a36
nyfelix <nyfelix@gmail.com>
2018-11-05 08:34:20 +0100

    New UI basically working
    Todo:
    - Switch Auto/Remote
    - Switch On/Off
    - Minimize (Webpack)
    - Use standard fonts
    - Rename all filenames to 8 characters

--- device-storage/www-root/js/main.js
+++ device-storage/www-root/js/main.js
@@ -1,1 +1,25 @@
+var app = function() {
+	//Init UI
+	var ip = "192.168.1.8"
 
+	// Walking
+	$("#bt-walk-front").click(()=>{
+		alert("front");
+	})
+	$("#bt-walk-stop").click(()=>{
+		alert("front");
+	})
+	$("#bt-walk-left").click(()=>{
+		alert("front");
+	})
+	$("#bt-walk-right").click(()=>{
+		alert("front");
+	})
+
+	//Sounds
+	$(".bt-sound").click((e)=> {
+    	// Do something /voice/play
+    	$.get( "http://" + ip + "/voice/play?track=" + e.target.name );
+	});
+
+}();
